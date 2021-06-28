e5405e7b715d9d750a6b09f2ad2b83607272214c
neilkuan <guan840912@gmail.com>
2021-06-28 01:38:23 +0000

    add new path

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,9 @@ const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.110.0',
   defaultReleaseBranch: 'main',
   name: 'tryEcsEc2AutoScaling',
+  authorName: 'Neil Kuab',
+  authorEmail: 'guan840912@gmail.com',
+  repository: 'https://github.com/neilkuan/ECS-EC2-Capacity-Provider-Demo.git',
   cdkDependencies: [
     '@aws-cdk/aws-ec2',
     '@aws-cdk/aws-ecs',
@@ -17,7 +20,7 @@ const project = new AwsCdkTypeScriptApp({
     },
   }),
   autoApproveOptions: {
-    secret: 'GITHUB_TOKEN',
+    secret: 'AUTOMATION_GITHUB_TOKEN',
     allowedUsernames: ['neilkuan'],
   },
 });
